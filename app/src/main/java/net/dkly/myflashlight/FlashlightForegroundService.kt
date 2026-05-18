a6943e7e6319588463786cc34124a268b2ace404
osscv <dkly2004@gmail.com>
2026-05-18 16:00:41 +0800

    Update FlashlightForegroundService.kt

--- app/src/main/java/net/dkly/myflashlight/FlashlightForegroundService.kt
+++ app/src/main/java/net/dkly/myflashlight/FlashlightForegroundService.kt
@@ -4,8 +4,8 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
-import android.app.ServiceInfo
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
